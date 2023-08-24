b0e8d70c642e396bc7d090dd65fe8c557447ce9d
ShiEnChan <chanshien@outlook.com>
2023-08-24 12:54:30 +0800

    Duplicate Application start date, rename start to end date

--- src/src/components/plan/createPlanComponent.js
+++ src/src/components/plan/createPlanComponent.js
@@ -210,7 +210,7 @@ function CreatePlan() {
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required
               />
-              <p className="text-sm">Application start date: {appEndDate}</p>
+              <p className="text-sm">Application end date: {appEndDate}</p>
             </div>
             <div>
               <label for="planColour" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
